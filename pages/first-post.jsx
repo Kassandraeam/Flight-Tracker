0831e071e4001564b690106e28cd6539606900ad
Kas Eam <KassandraEam@gmail.com>
2022-10-31 21:38:49 -0500

    WIP

--- pages/first-post.jsx
+++ pages/first-post.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
 
 const FirstPost = () => {
   return (
     <>
+    <Head>
+      <title>First Post</title>
+    </Head>
       <div>first-post</div>
       {/* {/* If I wanted to add a new route /authors/me, what would the filename would be? (including the directory)
       
@@ -12,13 +18,24 @@ const FirstPost = () => {
       <div>
         How do I make a new page?
       </div>
+      <h2>
+        <Link href={"/"}>Back home</Link>
+      </h2>
       <ol>
         <li>Create next app using the bootstrap command</li>
         <li>Go to pages folder</li>
         <li>create a new file. The file name will be what the url will be.</li>
         <li>Go to that url.</li>
-        <li>Test</li>
+        <li><Link href={"/link"}>Go to Link page.</Link></li>
       </ol>
+
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+
     </>
   )
 }
--- pages/link.jsx
+++ pages/link.jsx
@@ -5,6 +5,13 @@ const link = () => {
   return (
     <>
     <div>How to link:</div>
+    <ol>
+      <li>Import Link from 'next/link' at the top</li>
+      <li>Use the a tag</li>
+      <li>Welcome to <a href="/first-post">Next.js! Clicking this will take you back to the first post page.</a></li>
+      <li></li>
+      <li></li>
+    </ol>
     </>
   )
 }
